5d68c2250260fa6d66752e3a0f2e3af0571d4031
MohamedaSaaeed <116665263+MohamedaSaaeed@users.noreply.github.com>
2023-02-17 20:08:54 +0200

    Update create_ssh.tf

--- create_ssh.tf
+++ create_ssh.tf
@@ -2,5 +2,5 @@ resource "azurerm_ssh_public_key" "example" {
   name                = "example"
   resource_group_name = "example"
   location            = "West Europe"
-  public_key          = file("~/.ssh/id_rsa.pub")
+  public_key          = file("tf.pub")
 }
